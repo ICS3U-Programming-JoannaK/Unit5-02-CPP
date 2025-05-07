6d3c83283ecf629767d686777050ef649cae768e
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-05-07 16:50:07 +0000

    areaTriangle.cpp

---
+++ areaTriangle.cpp
@@ -0,0 +1,35 @@
+// Copyright 2025 Joanna Keza All rights reserved
+//
+// Created by: Joanna Keza
+// Data: May 6, 2025
+// This program uses a function that
+// calculates the area of a triangle
+
+#include<iostream>
+#include<string>
+
+void CalculateArea(float base, float height) {
+    float area = 1 / 2 * base * height;
+    std::cout << "The area is " << area << "cm^2" << std::endl;
+}
+
+int main() {
+    float heightInteger, baseInteger;
+    std::string heightString, baseString;
+
+    std::cout << "Enter the height of the triangle(cm): ";
+    std::cin >> heightString;
+    try {
+        heightInteger = std::stoi(heightString);
+        std::cout << "Enter the base of the triangle(cm): ";
+        std::cin >> baseString;
+        try {
+            baseInteger = std::stoi(baseString);
+            CalculateArea(baseInteger, heightInteger);
+        } catch (std::invalid_argument) {
+            std::cout << baseString << " is not a number.\n";
+        }
+    } catch (std::invalid_argument) {
+        std::cout << heightString << " is not a number.\n";
+    }
+}
